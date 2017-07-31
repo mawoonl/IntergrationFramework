d1a2b865b8a95b0fb645aeeb3b9738eba89abc52
Bob van der Valk <info@mawoo.nl>
2017-07-31 17:03:27 +0200

    scan plugins at startup

--- src/main/java/nl/mawoo/jerome/Framework.java
+++ src/main/java/nl/mawoo/jerome/Framework.java
@@ -1,5 +1,7 @@
 package nl.mawoo.jerome;
 
+import nl.mawoo.jerome.engine.Plugin;
+import nl.mawoo.jerome.engine.PluginLoader;
 import nl.mawoo.jerome.protocol.MawStreamHandlerFactory;
 import org.apache.log4j.Logger;
 
@@ -7,6 +9,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -20,9 +23,20 @@ public class Framework {
 
     public static void main(String[] args) throws IOException {
         logger.info("Mawoo Connector framework (c) 2017");
+        logger.info("----------------------------------");
         logger.info("Booting up");
         logger.info("Add protocol to system..");
         URL.setURLStreamHandlerFactory(new MawStreamHandlerFactory());
+
+        logger.info("Scanning for plugins");
+        PluginLoader loader = new PluginLoader("");
+        Map<String, Plugin> pluginMap = loader.mapPlugins();
+        for(Object o : pluginMap.entrySet()) {
+            Map.Entry entry = (Map.Entry) o;
+            Plugin plugin = (Plugin) entry.getValue();
+            logger.info("plugin: "+ plugin.getName() +" - "+ plugin.getDescription());
+        }
+
 
         Scanner in = new Scanner(System.in);
         logger.info("Ready for input");
